08bfc772016bfca20e6ffeaa365a3567ac48226c
JcksonMCD <jacksonmacdonald03@gmail.com>
2024-12-06 16:01:35 +0000

    Feat: Regex matching added for first name field.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // This will contain the JS for index.html - be sure to link to the correct file.
 const nameRegex = /^[\p{L}\p{M}'-]+(?: [\p{L}\p{M}'-]+)*$/u;
+const firstNameInput = document.getElementById("first-name");
 
+firstNameInput.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    if (nameRegex.exec(value)){
+        e.target.classList.add("valid");
+        e.target.classList.remove("invalid");     
+    } else {
+        e.target.classList.add("valid");
+        e.target.classList.remove("invalid"); 
+    }
+})
