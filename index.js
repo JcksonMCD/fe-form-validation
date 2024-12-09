84ef0543595d0e271071f5c12f9e663a5cffd8d8
JcksonMCD <jacksonmacdonald03@gmail.com>
2024-12-09 14:35:35 +0000

    Refactor: Check valid input method refactored to take regex as an input.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 // This will contain the JS for index.html - be sure to link to the correct file.
 const nameRegex = /^[\p{L}\p{M}'-]+(?: [\p{L}\p{M}'-]+)*$/u;
+const textRegex = /^[a-zA-Z0-9\s.,!?'"()-]+$/;
+
 const firstNameInput = document.getElementById("first-name");
 const lastNameInput = document.getElementById("last-name");
+const topText = document.getElementById("top-text");
 
-firstNameInput.addEventListener("input", checkValidInput);
-lastNameInput.addEventListener("input", checkValidInput);
+firstNameInput.addEventListener("input", (e) => checkValidInput(nameRegex, e));
+lastNameInput.addEventListener("input", (e) => checkValidInput(nameRegex, e));
 
-function checkValidInput(e){
+function checkValidInput(regex, e){
     const value = e.target.value;
 
-    if (nameRegex.exec(value)){
+    if (regex.exec(value)){
         e.target.classList.add("valid");
         e.target.classList.remove("invalid"); 
         e.target.style.borderColor = 'green';    
